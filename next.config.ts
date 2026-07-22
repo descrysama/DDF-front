e89c5c8b0cf037b5c441c5d9dfe34ccf5232bd41
Kaan <kaantopkaya32@gmail.com>
2026-07-22 17:59:42 +0200

    fix: fatal error

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'http', hostname: 'localhost', port: '1337', pathname: '/uploads/**' },
       { protocol: 'http', hostname: '84.235.236.136', port: '1337', pathname: '/uploads/**' },
+      { protocol: 'http', hostname: 'localhost', port: '1337', pathname: '/uploads/**' },
     ],
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
